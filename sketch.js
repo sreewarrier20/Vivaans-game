463668f44f1d260e2b6b66839d0afab025d0e570
sreewarrier20 <sreewarrier20@gmail.com>
2021-03-07 14:58:58 +0400

    we added a ground so that the catapult can be placed on it

--- sketch.js
+++ sketch.js
@@ -18,6 +18,7 @@ function setup(){
     
     ground = new Ground(600,height,1200,20)
 
+    ground2=new Ground(200,285,250,215)
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
@@ -48,6 +49,7 @@ function draw(){
     ground.display();
     pig1.display();
     log1.display();
+ground2.display();
 
     box3.display();
     box4.display();
